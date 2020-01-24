052f32320a625a82024b0b27ab77025eaa5929da
Alexandr Vetrov <vetrov@icc.ru>
2020-01-25 01:07:35 +0800

    original

---
+++ src/environments/environment.interface.ts
@@ -0,0 +1,8 @@
+/**
+ * App environment
+ */
+export interface Environment {
+  production: boolean;
+  version?: string;
+  apiUrl?: string;
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,12 @@
+import { Environment } from './environment.interface';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+export const environment: Environment = {
   production: false,
-  apiUrl: 'http://example.com/api/',
+  apiUrl: 'http://development.com/api/',
 };
 
 /*
